8df51fde00fd6694fcc73ddc38c473c6d2d6f51c
gwony <moonsky1124@naver.com>
2025-01-08 00:58:08 +0900

    wiseSaying t3 r

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,8 +3,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
 
@@ -40,7 +41,27 @@ public class AppTest {
         // then
         System.setOut(originalOut);
         assertThat(out.toString()).
-                contains("== 명언 앱 ==")
-                .contains("명령) ");
+                containsSubsequence("== 명언 앱 ==", "명령) ");
+    }
+
+    @DisplayName("종료 입력 :  시스템을 종료합니다. 출력. 다만 == 명언 앱 ==과 명령) 다음에 출력되어야 한다.")
+    @Test
+    void t() {
+        // given
+        Scanner sc = new Scanner("종료\n");
+
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out)); // 표준 출력 캡처
+
+        // when
+        App app = new App();
+        app.run();
+
+        // then
+        System.setOut(originalOut);
+        assertThat(out.toString())
+                .containsSubsequence("== 명언 앱 ==", "명령) ")
+                .contains("시스템을 종료합니다.");
     }
 }
