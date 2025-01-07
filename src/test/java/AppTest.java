9072213278b4c9321900c09e5df9e63919ae0c97
gwony <moonsky1124@naver.com>
2025-01-08 02:10:34 +0900

    wiseSaying t4 r

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,34 +1,58 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class AppTest {
-
-    @DisplayName("시작 시 출력 : == 명언 앱 == ")
     @Test
+    @DisplayName("시작 시 출력 : == 명언 앱 == ")
     void t1() {
         // given
         String out = TestBoot.run("종료");
+
+        // then
         assertThat(out).contains("== 명언 앱 ==");
     }
 
-    @DisplayName("시작 시 출력2 : 명령) ")
     @Test
+    @DisplayName("시작 시 출력2 : 명령) ")
     void t2() {
+        // given
         String out = TestBoot.run("종료");
+
+        // then
         assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명령) ");
     }
 
-    @DisplayName("종료 입력 :  시스템을 종료합니다. 출력. 다만 == 명언 앱 ==과 명령) 다음에 출력되어야 한다.")
     @Test
+    @DisplayName("종료 입력 :  시스템을 종료합니다. 출력. 다만 == 명언 앱 ==과 명령) 다음에 출력되어야 한다.")
     void t3() {
         // given
         String out = TestBoot.run("종료");
 
+        // then
         assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명령) ")
                 .contains("시스템을 종료합니다.");
     }
+
+    @Test
+    @DisplayName("")
+    void t4() {
+        // given
+        String out = TestBoot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        // then
+        assertThat(out)
+                .containsSubsequence("명언 : ", "작가 : ");
+      }
+
+
+
 }
