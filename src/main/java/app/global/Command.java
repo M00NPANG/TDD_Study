992396480401ec22da2c1d92e3936859347bb162
gwony <moonsky1124@naver.com>
2025-01-08 17:39:23 +0900

    command t2 g

--- src/main/java/app/global/Command.java
+++ src/main/java/app/global/Command.java
@@ -1,12 +1,14 @@
 package app.global;
 
 public class Command {
-    public String getActionName(String s) {
-        return "";
-    }
+
+    String actionName;
     // 명령어?=id=1  -> 명령어 |?| id=1 분리해서 반환하는 기능
-//    String[] cmdBits = cmd.split("\\?");
-//    String actionName = cmdBits[0];
+    public String getActionName(String s) {
+        String[] paramBits = s.split("\\?");
+        actionName = paramBits[0];
+        return  actionName;
+    }
 
     // id=1 -> id |=| 1 분리하고 정수로 변환해서 반화하는 기능
 }
