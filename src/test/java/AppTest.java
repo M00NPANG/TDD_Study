4070f302f581ec31bc94b5565e64a20cd86ca0a9
gwony <moonsky1124@naver.com>
2025-01-08 10:01:46 +0900

    wiseSaying t7 r,g

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class AppTest {
@@ -87,6 +89,40 @@ public class AppTest {
                 .count();
 
         assertThat(commandCount).isEqualTo(3);
+
     }
+
+    @Test
+    @DisplayName("2단계 : 명언 2개 등록, 명언) 다음 명언 : 과 작가 : 가 2번씩 출력되어야 한다.")
+    void t7() {
+        // given
+        String out = TestBoot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        // then
+        List<String> lines = out.lines().toList();
+
+        int occurrences = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).startsWith("명령) ")) {
+                // "명령)", "명언 :", "작가 :" 순서 확인
+                if (i + 2 < lines.size() &&
+                        lines.get(i + 1).startsWith("명언 : ") &&
+                        lines.get(i + 2).startsWith("작가 : ")) {
+                    occurrences++;
+                    i += 2; // 확인한 범위는 건너뛰기
+                }
+            }
+        }
+
+        assertThat(occurrences).isEqualTo(2); // 패턴이 2번 반복되었는지 확인
+    }
+
 
 }
